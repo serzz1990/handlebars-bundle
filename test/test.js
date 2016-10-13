59077a3345b826508724d432fa37830c683756db
Sergey Savin <Sergey@Savin>
2016-10-13 14:41:11 +0300

    add test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const bundle = require('../index');
+
+bundle.build({
+	src : process.cwd() + '/test/section/',
+	root: process.cwd() + '/test/',
+	watch: process.argv.indexOf('--watch') !== -1,
+	ignore_errors: false
+});
