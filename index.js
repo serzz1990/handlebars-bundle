7b81013b4e4a06d38828ee5b9d52bddddc710eeb
Sergey Savin <Sergey@Savin>
2017-09-22 14:40:02 +0300

    Остановка сборки при наличии ошибки

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var glob   = require('glob');
-var chalk  = require('chalk');
-var path   = require('path');
-var fs     = require('fs');
-var mkpath = require('mkpath');
-var Promise = require('es6-promise').Promise;
-var RecursiveWatch = require('recursive-watch');
+const glob   = require('glob');
+const chalk  = require('chalk');
+const path   = require('path');
+const fs     = require('fs');
+const mkpath = require('mkpath');
+const Promise = require('es6-promise').Promise;
+const RecursiveWatch = require('recursive-watch');
 
-var EXT = '.hbs';
-var BUNDLE_EXT = '.bundle.json';
-var timeReBuild = 600;
+const EXT = '.hbs';
+const BUNDLE_EXT = '.bundle.json';
+const timeReBuild = 600;
 
 
 function build (options) {
@@ -30,14 +30,14 @@ function build (options) {
             }
 
             files.forEach(function (file) {
-                var name = file.substr(0, file.length - 4).replace(options.root, '');
-                var path_template = name.split('/');
+                let name = file.substr(0, file.length - 4).replace(options.root, '');
+                let path_template = name.split('/');
 
-                var template_name = path_template.pop();
+                let template_name = path_template.pop();
                 path_template = path_template.join('/');
 
-                var output = path.join(options.output, path_template + '/');
-                var bundle = JSON.stringify(put_together_template(options.root, name, options));
+                let output = path.join(options.output, path_template + '/');
+                let bundle = JSON.stringify(put_together_template(options.root, name, options));
 
                 mkpath.sync(output);
                 fs.writeFileSync(path.join(output, template_name +  BUNDLE_EXT), bundle);
@@ -54,8 +54,8 @@ function build (options) {
 
 function watch (options) {
 
-    var ext = new RegExp('\\'+ EXT +'$');
-    var _timeout;
+    let ext = new RegExp('\\'+ EXT +'$');
+    let _timeout;
 
     options = parse_options(options);
 
@@ -76,13 +76,13 @@ function watch (options) {
 
 function get_folders (src, cb) {
 
-    var folders = [];
+    let folders = [];
 
     glob(src, null, function (err, files) {
 
         files.forEach(function (file) {
 
-            var folder = file.split('/');
+            let folder = file.split('/');
             folder.pop();
             folder = folder.join('/') + '/';
 
@@ -96,7 +96,7 @@ function get_folders (src, cb) {
 
     });
 
-};
+}
 
 function parse_options (options = {}) {
 
@@ -111,7 +111,7 @@ function parse_options (options = {}) {
 function put_together_template (path_prefix, template_name, options, seen = {}) {
 
     let template_path   = path.join(path_prefix, template_name + EXT);
-    let content         = getFileContent(template_path, template_name);
+    let content         = getFileContent(template_path, template_name, options);
 
     let res = {
         template: {},
@@ -128,7 +128,7 @@ function put_together_template (path_prefix, template_name, options, seen = {}) 
 
     getPartialsByContent(content).forEach(function (path) {
 
-        var sub_template_name = path;
+        let sub_template_name = path;
 
         if (seen[sub_template_name]) {
             return;
@@ -136,12 +136,12 @@ function put_together_template (path_prefix, template_name, options, seen = {}) 
             seen[sub_template_name] = 1;
         }
 
-        var sub_template = put_together_template(path_prefix, sub_template_name, options, seen);
+        let sub_template = put_together_template(path_prefix, sub_template_name, options, seen);
 
         if (sub_template) {
             res.partials[sub_template.template.name] = sub_template.template;
 
-            for (var partial in sub_template.partials) {
+            for (let partial in sub_template.partials) {
                 if (sub_template.partials.hasOwnProperty(partial)) {
                     res.partials[partial] = sub_template.partials[partial];
                     seen[partial] = 1;
@@ -157,7 +157,7 @@ function put_together_template (path_prefix, template_name, options, seen = {}) 
 
 }
 
-function getFileContent (path, partial_name) {
+function getFileContent (path, partial_name, options) {
 
     try {
 
@@ -167,7 +167,7 @@ function getFileContent (path, partial_name) {
     catch (e) {
 
         if (partial_name.search('/') > -1) {
-            console.error(e.message);
+            error(e.message, options);
         }
 
         return null;
@@ -194,7 +194,7 @@ function getPartialsByContent (content) {
 
 function error (err, options) {
 
-    console.log(chalk.red('Combiner Build: Error:'));
+    console.log(chalk.red('Combiner build error:'));
     console.log(chalk.red(err));
 
     if (!options.ignore_errors){
